9461964cca82cf64013346d5884fecedb01210b5
kristof-eekhaut <ke>
2016-09-17 11:38:27 +0200

    Implement Geographical to GeoCentric transformation

---
+++ src/main/java/com/eekhaut/kristof/coordinatetransform/algorithm/GeographicalToGeocentricCoordinateTransformation.java
@@ -0,0 +1,104 @@
+package com.eekhaut.kristof.coordinatetransform.algorithm;
+
+public class GeographicalToGeocentricCoordinateTransformation {
+
+    private final double a;
+    private final double f;
+
+    private double eSquared;
+    private double e;
+
+    public GeographicalToGeocentricCoordinateTransformation(double a, double f) {
+        this.a = a;
+        this.f = f;
+
+        initCommonValues();
+    }
+
+    private void initCommonValues() {
+        eSquared = 2.0 * f - Math.pow(f, 2);
+        e = Math.sqrt(eSquared);
+    }
+
+    /**
+     * @param phi in radians
+     * @param lambda in radians
+     * @param h in meters
+     * @return Result
+     */
+    public Result directTransformation(double phi, double lambda, double h) {
+        double v = a / Math.sqrt(1 - eSquared * Math.pow(Math.sin(phi), 2));
+
+        double x = (v + h) * Math.cos(phi) * Math.cos(lambda);
+        double y = (v + h) * Math.cos(phi) * Math.sin(lambda);
+        double z = ((1 - eSquared) * v + h) * Math.sin(phi);
+
+        return new Result(x, y, z, phi, lambda, h);
+    }
+
+    /**
+     * @param x in meters
+     * @param y in meters
+     * @param z in meters
+     * @return Result
+     */
+    public Result inverseTransformation(double x, double y, double z) {
+        double pSquared = Math.pow(x, 2) + Math.pow(y, 2);
+        double p = Math.sqrt(pSquared);
+
+        double r = Math.sqrt(pSquared + Math.pow(z, 2));
+        double u = Math.atan((z / p) * ((1 - f) + (eSquared * a / r)));
+
+        double lambda = Math.atan(y / x);
+        double phi = Math.atan( (z * (1 - f) + eSquared * a * Math.pow(Math.sin(u), 3)) / ((1 - f) * (p - eSquared * a * Math.pow(Math.cos(u), 3))) );
+        double h = p * Math.cos(phi) + z * Math.sin(phi) - a * Math.sqrt(1 - eSquared * Math.pow(Math.sin(phi), 2));
+
+        return new Result(x, y, z, phi, lambda, h);
+    }
+
+    public static class Result {
+        private double x, y, z;
+        private double phi, lambda, h;
+
+        Result(double x, double y, double z, double phi, double lambda, double h) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.phi = phi;
+            this.lambda = lambda;
+            this.h = h;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public double getZ() {
+            return z;
+        }
+
+        public double getPhi() {
+            return phi;
+        }
+
+        public double getLatitude() {
+            return Math.toDegrees(phi);
+        }
+
+        public double getLambda() {
+            return lambda;
+        }
+
+        public double getLongitude() {
+            return Math.toDegrees(lambda);
+        }
+
+        public double getH() {
+            return h;
+        }
+    }
+}
---
+++ src/test/java/com/eekhaut/kristof/coordinatetransform/algorithm/GeographicalToGeocentricCoordinateTransformationTest.java
@@ -0,0 +1,87 @@
+package com.eekhaut.kristof.coordinatetransform.algorithm;
+
+import com.eekhaut.kristof.coordinatetransform.util.DegreeUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static com.eekhaut.kristof.coordinatetransform.util.DegreeUtils.toDegrees;
+import static java.lang.Math.toRadians;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class GeographicalToGeocentricCoordinateTransformationTest {
+
+    private GeographicalToGeocentricCoordinateTransformation transformation = new GeographicalToGeocentricCoordinateTransformation(6378388D, 1.0 / 297.0);
+
+    private String description;
+    private double latitude, longitude, h;
+    private double x, y, z;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"Example", toDegrees(50, 51, 48, 752), toDegrees(3, 49, 8, 255), 76549.0, 4025100.583, 268684.735, 4924132.152},
+        });
+    }
+
+    public GeographicalToGeocentricCoordinateTransformationTest(
+            String description,
+            double latitude,
+            double longitude,
+            double h,
+            double x,
+            double y,
+            double z) {
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.h = h;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Test
+    public void test() {
+
+        double phi = toRadians(latitude);
+        double lambda = toRadians(longitude);
+
+        GeographicalToGeocentricCoordinateTransformation.Result result = transformation.directTransformation(phi, lambda, h);
+
+        double actualX = result.getX();
+        double actualY = result.getY();
+        double actualZ = result.getZ();
+
+        System.out.println("X: " + actualX + " (expected: " + x + ")");
+        System.out.println("Y: " + actualY + " (expected: " + y + ")");
+        System.out.println("Z: " + actualZ + " (expected: " + z + ")");
+
+        assertEquals(x, actualX, 0.001);
+        assertEquals(y, actualY, 0.001);
+        assertEquals(z, actualZ, 0.001);
+    }
+
+
+    @Test
+    public void testInverse() {
+
+        GeographicalToGeocentricCoordinateTransformation.Result result = transformation.inverseTransformation(x, y, z);
+
+        double actualLatitude = result.getLatitude();
+        double actualLongitude = result.getLongitude();
+        double actualH = result.getH();
+
+        System.out.println("Latitude: " + actualLatitude + " (expected: " + latitude + ")");
+        System.out.println("Longitude: " + actualLongitude + " (expected: " + longitude + ")");
+        System.out.println("h: " + actualH + " (expected: " + h + ")");
+
+        assertEquals(latitude, actualLatitude, 0.000001);
+        assertEquals(longitude, actualLongitude, 0.000001);
+        assertEquals(h, actualH, 0.001);
+    }
+}
